df26c3d0a44088b85fd39590d1e6d1c445505bee
Takuya Azumaguchi <catchball_8@hotmail.com>
2018-12-22 11:05:49 +0900

    Initial Commit

---
+++ NextWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextWeather
+//
+//  Created by 東口拓也 on 2018/12/22.
+//  Copyright © 2018 TakuyaAzumaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
